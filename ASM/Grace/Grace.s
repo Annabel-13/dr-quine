db4c53814a17b036e9285070672c70c1e4df635e
Hanna <55405919+Annabel-13@users.noreply.github.com>
2025-05-28 18:19:02 +0200

    Update Grace.s
    right version of Grace in ASM with 3 macro and no main function

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,39 +1,36 @@
-; Only one comment
-
-%define OPEN_CALL_NB 2        ; SYS_OPEN (Linux)
-%define CLOSE_CALL_NB 3       ; SYS_CLOSE (Linux)
-%define O_RDWR 2              ; O_RDWR flag
-%define O_CREAT 64            ; O_CREAT flag
-%define PERMISSIONS 420       ; 0644 in octal (rw-r--r--)
-
+; A comment
+%define S "; A comment%1$c%%define S %2$c%3$s%2$c%1$c%%define F %2$cGrace_kid.s%2$c%1$c%%define G() extern fopen, fprintf, fclose%1$c%%macro MAIN 0%1$cglobal main%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, [rel filename]%1$c    lea rsi, [rel mode]%1$c    call fopen%1$c    mov [rsp], rax%1$c    mov rdi, rax%1$c    lea rsi, [rel fmt]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    lea r8, [rel fmt]%1$c    call fprintf%1$c    mov rdi, [rsp]%1$c    call fclose%1$c    xor eax, eax%1$c    add rsp, 16%1$c    pop rbp%1$c    ret%1$c%%endmacro%1$csection .data%1$c    fmt db S, 0%1$c    filename db F, 0%1$c    mode db %2$cw%2$c, 0%1$csection .text%1$cG()%1$cMAIN%1$c"
+%define F "Grace_kid.s"
+%define G() extern fopen, fprintf, fclose
+%macro MAIN 0
 global main
-extern printf
-
-section .rodata
-output db "Grace_kid.s", 0
-code_str db "; Only one comment%1$c%%define OPEN_CALL_NB 2%1$c%%define CLOSE_CALL_NB 3%1$c%%define O_RDWR 2%1$c%%define O_CREAT 64%1$c%%define PERMISSIONS 420%1$c%1$cglobal main%1$cextern printf%1$c%1$csection .rodata%1$coutput db %2$cGrace_kid.s%2$c, 0%1$ccode_str db %2$c%3$s%2$c, 0%1$c%1$csection .text%1$c%1$cmain:%1$center 0, 0%1$cmov rax, OPEN_CALL_NB%1$clea rdi, [rel output]%1$cmov rsi, O_RDWR | O_CREAT%1$cmov rdx, PERMISSIONS%1$csyscall%1$cmov rdi, rax%1$clea rsi, [rel code_str]%1$ccall printf%1$cmov rax, CLOSE_CALL_NB%1$cmov rdi, rdi%1$csyscall%1$cleave%1$cret%1$c", 0
-
-section .text
-
 main:
-    enter 0, 0
-    ; open("Grace_kid.s", O_RDWR | O_CREAT, 0644)
-    mov rax, OPEN_CALL_NB
-    lea rdi, [rel output]
-    mov rsi, O_RDWR | O_CREAT
-    mov rdx, PERMISSIONS
-    syscall
-
-    ; call printf(code_str)
-    lea rdi, [rel code_str]
-    call printf
-
-    ; close(fd)
-    mov rax, CLOSE_CALL_NB
+    push rbp
+    mov rbp, rsp
+    sub rsp, 16
+    lea rdi, [rel filename]
+    lea rsi, [rel mode]
+    call fopen
+    mov [rsp], rax
     mov rdi, rax
-    syscall
-
-    leave
+    lea rsi, [rel fmt]
+    mov rdx, 10
+    mov rcx, 34
+    lea r8, [rel fmt]
+    call fprintf
+    mov rdi, [rsp]
+    call fclose
+    xor eax, eax
+    add rsp, 16
+    pop rbp
     ret
+%endmacro
+section .data
+    fmt db S, 0
+    filename db F, 0
+    mode db "w", 0
+section .text
+G()
+MAIN
 
 section .note.GNU-stack noalloc noexec nowrite progbits
