507fa39d09115da85ea558d17043ef07c7e3115d
Hanna <55405919+Annabel-13@users.noreply.github.com>
2025-05-30 17:36:49 +0200

    Update Grace.s
    hide main in macro

--- ASM/Grace/Grace.s
+++ ASM/Grace/Grace.s
@@ -1,36 +1,39 @@
-; A comment
-%define S "; A comment%1$c%%define S %2$c%3$s%2$c%1$c%%define F %2$cGrace_kid.s%2$c%1$c%%define G() extern fopen, fprintf, fclose%1$c%%macro MAIN 0%1$cglobal main%1$cmain:%1$c    push rbp%1$c    mov rbp, rsp%1$c    sub rsp, 16%1$c    lea rdi, [rel filename]%1$c    lea rsi, [rel mode]%1$c    call fopen wrt ..plt%1$c    mov [rsp], rax%1$c    mov rdi, rax%1$c    lea rsi, [rel fmt]%1$c    mov rdx, 10%1$c    mov rcx, 34%1$c    lea r8, [rel fmt]%1$c    call fprintf wrt ..plt%1$c    mov rdi, [rsp]%1$c    call fclose wrt ..plt%1$c    xor eax, eax%1$c    add rsp, 16%1$c    pop rbp%1$c    ret%1$c%%endmacro%1$csection .data%1$c    fmt db S, 0%1$c    filename db F, 0%1$c    mode db %2$cw%2$c, 0%1$csection .text%1$cG()%1$cMAIN%1$c%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c"
+; Grace
+%define S "; Grace%1$c%%define S %2$c%3$s%2$c%1$c%%define F %2$cGrace_kid.s%2$c%1$c%%define M global main%1$cmain:%1$c push rbp%1$c mov rbp, rsp%1$c sub rsp, 4112%1$c mov rax, 2%1$c lea rdi, [rel f]%1$c mov rsi, 577%1$c mov rdx, 420o%1$c syscall%1$c mov r12, rax%1$c lea rdi, [rsp]%1$c lea rsi, [rel s]%1$c mov rdx, 10%1$c mov rcx, 34%1$c lea r8, [rel s]%1$c call sprintf wrt ..plt%1$c mov rdx, rax%1$c mov rax, 1%1$c mov rdi, r12%1$c lea rsi, [rsp]%1$c syscall%1$c mov rax, 3%1$c mov rdi, r12%1$c syscall%1$c xor eax, eax%1$c add rsp, 4112%1$c pop rbp%1$c ret%1$csection .data%1$c s db S, 0%1$c f db F, 0%1$csection .text%1$cextern sprintf%1$cM%1$csection .note.GNU-stack noalloc noexec nowrite progbits%1$c"
 %define F "Grace_kid.s"
-%define G() extern fopen, fprintf, fclose
-%macro MAIN 0
-global main
+%define M global main
 main:
-    push rbp
-    mov rbp, rsp
-    sub rsp, 16
-    lea rdi, [rel filename]
-    lea rsi, [rel mode]
-    call fopen wrt ..plt
-    mov [rsp], rax
-    mov rdi, rax
-    lea rsi, [rel fmt]
-    mov rdx, 10
-    mov rcx, 34
-    lea r8, [rel fmt]
-    call fprintf wrt ..plt
-    mov rdi, [rsp]
-    call fclose wrt ..plt
-    xor eax, eax
-    add rsp, 16
-    pop rbp
-    ret
-%endmacro
+ push rbp
+ mov rbp, rsp
+ sub rsp, 4112
+ mov rax, 2
+ lea rdi, [rel f]
+ mov rsi, 577
+ mov rdx, 420o
+ syscall
+ mov r12, rax
+ lea rdi, [rsp]
+ lea rsi, [rel s]
+ mov rdx, 10
+ mov rcx, 34
+ lea r8, [rel s]
+ call sprintf wrt ..plt
+ mov rdx, rax
+ mov rax, 1
+ mov rdi, r12
+ lea rsi, [rsp]
+ syscall
+ mov rax, 3
+ mov rdi, r12
+ syscall
+ xor eax, eax
+ add rsp, 4112
+ pop rbp
+ ret
 section .data
-    fmt db S, 0
-    filename db F, 0
-    mode db "w", 0
+ s db S, 0
+ f db F, 0
 section .text
-G()
-MAIN
-
+extern sprintf
+M
 section .note.GNU-stack noalloc noexec nowrite progbits
