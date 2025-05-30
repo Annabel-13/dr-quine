60d28bd0d18688b6127b01466b3491cfb5901b61
Hanna <55405919+Annabel-13@users.noreply.github.com>
2025-05-30 16:26:11 +0200

    Update Grace.c
    add another define for subject

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -2,6 +2,7 @@
 Comment outside
 */
 #include <stdio.h>
-#define S "/*%cComment outside%c*/%c#include <stdio.h>%c#define S %c%s%c%c#define F(x) int main(){FILE *fp=fopen(%cGrace_kid.c%c,%cw%c);fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,34,34,10,10);fclose(fp);}%cF(x)%c"
-#define F(x) int main(){FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,34,34,10,10);fclose(fp);}
-F(x)
+#define S "/*%cComment outside%c*/%c#include <stdio.h>%c#define S %c%s%c%c#define F int main(){FILE *fp=fopen(%cGrace_kid.c%c,%cw%c);fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,34,34,10,10,10);fclose(fp);return 0;}%c#define G F%cG%c"
+#define F int main(){FILE *fp=fopen("Grace_kid.c","w");fprintf(fp,S,10,10,10,10,34,S,34,10,34,34,34,34,10,10,10);fclose(fp);return 0;}
+#define G F
+G
